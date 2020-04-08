78d9a4aa82ce54883e6951fed7bcfad87f86f57b
alidav173 <55164581+alidav173@users.noreply.github.com>
2020-04-07 21:01:36 -0300

    Add files via upload
    Proyecto obligatorio 1 para el curso Reactjs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Todoform from './components/Todoform';
+
+function App() {
+  return (
+    <div className="App">
+      <Todoform />
+    </div>
+  );
+}
+
+export default App;
